ea4e0e289663f73a2cba375b9c3d37c9a9e35c9f
Kyaw Tun <kyawtun@yathit.com>
2014-10-03 16:37:14 +0800

    reorg

---
+++ src/ydn/crm/tracking/include.js
@@ -0,0 +1,15 @@
+/**
+ * Created by kyawtun on 30/9/14.
+ */
+
+goog.require('ydn.crm.TrackingService');
+goog.require('ydn.api.User');
+goog.require('ydn.base');
+goog.require('ydn.client.RichClient');
+goog.require('ydn.db.Storage');
+goog.require('ydn.db.sync');
+goog.require('ydn.db.sync.Entity');
+goog.require('ydn.debug');
+goog.require('ydn.shared.api.UserDisplayImpl');
+goog.require('ydn.testing');
+
---
+++ src/ydn/crm/tracking/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by kyawtun on 30/9/14.
+ */
+
+
+ydn.debug.log('ydn.db.sync.Entity', 'finest');
+var app = new TrackingApp();
+app.run();
