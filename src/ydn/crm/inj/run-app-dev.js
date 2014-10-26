e9723a1ea757709c6b160d16940e56d91cd9a720
Kyaw Tun <kyawtun@yathit.com>
2014-10-26 22:13:12 +0800

    app with gmail fixture

--- src/ydn/crm/inj/run-app-dev.js
+++ src/ydn/crm/inj/run-app-dev.js
@@ -9,23 +9,28 @@ ydn.debug.log('ydn.crm.inj.App', 'finest');
 // ydn.msg.Pipe.DEBUG = true;
 
 var app = ydn.crm.inj.App.runInjApp();
+var mock_content = document.getElementById('mock-content');
 
 
-document.getElementById('go-email').onclick = function(e) {
-  var main = document.querySelector('table[role="presentation"]');
-  if (main) {
-    main.parentElement.removeChild(main);
+document.getElementById('go-inbox').onclick = function(e) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'fixture/gmail-inbox.html', false);
+  xhr.onload = function() {
+    mock_content.innerHTML = xhr.responseText;
     location.hash = '';
-  } else {
-    main = document.createElement('div');
-    main.innerHTML = '<table role="presentation"><tr>' +
-        '<td></td><td></td><td style="height: 624px;">' +
-        '<table><tbody><tr><td><img jid="some@example.com"/>' +
-        '</td></tr></tbody>' +
-        '</table></td></tr></table>';
-    document.body.appendChild(main);
+  };
+  xhr.send();
+};
+
+document.getElementById('go-thread').onclick = function(e) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'fixture/gmail-thread-2.html', false);
+  xhr.onload = function() {
+    mock_content.innerHTML = xhr.responseText;
     location.hash = 'inbox/2345675435';
-  }
+  };
+  xhr.send();
+
 };
 
 
