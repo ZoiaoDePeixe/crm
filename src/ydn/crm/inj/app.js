5726eb4d08e9906375f150f3c9f0f65a1d63211c
Kyaw Tun <kyawtun@yathit.com>
2014-11-11 15:00:24 +0800

    neat up.

--- src/ydn/crm/inj/app.js
+++ src/ydn/crm/inj/app.js
@@ -52,8 +52,6 @@ goog.require('ydn.msg.Pipe');
  */
 ydn.crm.inj.App = function() {
 
-  ydn.ui.setTemplateDocument(chrome.extension.getURL(ydn.crm.base.INJ_TEMPLATE));
-
   // connection channel with background page.
   ydn.msg.initPipe(ydn.msg.ChannelName.CONTENT_SCRIPT);
 
@@ -198,6 +196,8 @@ ydn.crm.inj.App.runInjApp = function() {
   ydn.crm.shared.init();
 
   var app = new ydn.crm.inj.App();
+
+  ydn.ui.setTemplateDocument(chrome.extension.getURL(ydn.crm.base.INJ_TEMPLATE));
 
   var tid2 = window.setTimeout(function() {
 
