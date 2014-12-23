34d12e7e395794d4d9c6f4d63d846a022d537cdc
Kyaw Tun <kyawtun@yathit.com>
2014-12-23 12:45:33 +0800

    unit test fix.

---
+++ src/ydn/social/index.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Social module.
+ *
+ * @author kyawtun@yathit.com (Kyaw Tun)
+ */
+
+
+goog.provide('ydn.so');
+
+
+/**
+ * @const
+ * @type {string}
+ */
+ydn.so.STORE_NAME = 'Social';
