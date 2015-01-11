7c6aa23713666d8277bdb76bcf0281f46d9ce8e9
Kyaw Tun <kyawtun@yathit.com>
2015-01-11 22:57:22 +0800

    update for meeting record

--- src/ydn/crm/sugarcrm/index.js
+++ src/ydn/crm/sugarcrm/index.js
@@ -314,7 +314,8 @@ ydn.crm.sugarcrm.fixSugarCrmModuleMeta = function(info) {
     }
 
     // fix group assignment
-    if ([ydn.crm.sugarcrm.ModuleName.CALLS, ydn.crm.sugarcrm.ModuleName.TASKS].indexOf(info.module_name) >= 0 &&
+    if ([ydn.crm.sugarcrm.ModuleName.CALLS, ydn.crm.sugarcrm.ModuleName.MEETINGS,
+          ydn.crm.sugarcrm.ModuleName.TASKS].indexOf(info.module_name) >= 0 &&
         ['date_start', 'date_end', 'date_due', 'duration_hours', 'duration_minutes'
         ].indexOf(name) >= 0) {
       mf.group = 'appointment';
