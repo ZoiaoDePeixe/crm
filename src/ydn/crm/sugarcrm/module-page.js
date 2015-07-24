4efe7e868592afd9f71107370578f9870c29a9ee
Kyaw Tun <kyawtun@yathit.com>
2015-07-25 03:59:55 +0800

    heading

--- src/ydn/crm/sugarcrm/module-page.js
+++ src/ydn/crm/sugarcrm/module-page.js
@@ -26,9 +26,12 @@
 goog.provide('ydn.crm.su.ui.ModulePage');
 goog.require('goog.ui.Button');
 goog.require('goog.ui.CheckBoxMenuItem');
+goog.require('goog.ui.Component');
+goog.require('goog.ui.Css3ButtonRenderer');
+goog.require('goog.ui.Css3MenuButtonRenderer');
+goog.require('goog.ui.CustomButton');
 goog.require('goog.ui.Menu');
 goog.require('goog.ui.MenuButton');
-goog.require('goog.ui.Component');
 goog.require('goog.ui.Toolbar');
 goog.require('wgui.TextInput');
 goog.require('ydn.crm.ui.events');
@@ -100,18 +103,13 @@ ydn.crm.su.ui.ModulePage.prototype.createDom = function() {
 
   var symbol = ydn.crm.su.toModuleSymbol(this.name);
 
-  var m_menu = new goog.ui.Menu(dom);
-  var modules = model.listModules();
-  for (var i = 0; i < modules.length; i++) {
-    var m_item = new goog.ui.CheckBoxMenuItem(modules[i], modules[i], dom);
-    if (modules[i] == this.name) {
-      m_item.setChecked(true);
-    }
-    m_menu.addChild(m_item, true);
-  }
-  var m_btn = new goog.ui.MenuButton(symbol, m_menu, null, dom);
+  var css_rdr = goog.ui.Css3ButtonRenderer.getInstance();
+  var css_mbr = goog.ui.Css3MenuButtonRenderer.getInstance();
+  var m_btn = new goog.ui.Button('New', css_rdr, dom);
+  m_btn.setTooltip('Create a new record');
   this.toolbar_.addChild(m_btn, true);
 
+  /*
   var f_menu = new goog.ui.Menu(dom);
   f_menu.addChild(new goog.ui.CheckBoxMenuItem('All records', 'all', dom), true);
   var my_item = new goog.ui.CheckBoxMenuItem('My records', 'my', dom);
@@ -119,21 +117,36 @@ ydn.crm.su.ui.ModulePage.prototype.createDom = function() {
   f_menu.addChild(my_item, true);
   f_menu.addChild(new goog.ui.CheckBoxMenuItem('Favorites', 'favorites', dom), true);
   f_menu.addChild(new goog.ui.CheckBoxMenuItem('Cache', 'cache', dom), true);
-  var filter = new goog.ui.MenuButton('Filter', f_menu, null, dom);
+  var filter = new goog.ui.MenuButton('Filter', f_menu, css_mbr, dom);
   this.toolbar_.addChild(filter, true);
+  */
+
   var search = new wgui.TextInput('', null, dom);
   this.toolbar_.addChild(search, true);
 
+  var svg = ydn.crm.ui.createSvgIcon('search');
+  var search_btn = new goog.ui.CustomButton(svg, null, dom);
+  var combo_el = document.createElement('span');
+  combo_el.classList.add('search-button');
+  this.toolbar_.getContentElement().appendChild(combo_el);
+  // this.toolbar_.addChild(search_btn, true);
+  this.toolbar_.addChild(search_btn, false);
+  search_btn.render(combo_el);
+  search_btn.addClassName('goog-custom-button-collapse-right');
   var s_menu = new goog.ui.Menu(dom);
   s_menu.addChild(new goog.ui.CheckBoxMenuItem('Id', 'id', dom), true);
   var r_item = new goog.ui.CheckBoxMenuItem('Recent', 'recent', dom);
   r_item.setChecked(true);
   s_menu.addChild(r_item, true);
   s_menu.addChild(new goog.ui.CheckBoxMenuItem('Name', 'name', dom), true);
-  var sort = new goog.ui.MenuButton('Sort', s_menu, null, dom);
-  this.toolbar_.addChild(sort, true);
-
+  var sort = new goog.ui.MenuButton(null, s_menu, null, dom);
+  sort.setPositionElement(search_btn.getElement());
+  sort.addClassName('goog-menu-button-collapse-left');
+  this.toolbar_.addChild(sort, false);
+  sort.render(combo_el);
+  sort.setTooltip('Modify filter and sort order');
   this.toolbar_.render(head);
+
 };
 
 
